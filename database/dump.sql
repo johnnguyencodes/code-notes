af4d44869081ab396eae3ef02cb134ffd62911b7
Adam Botma <adam.botma@me.com>
2020-08-12 16:51:19 +0000

    refeshed database

--- database/dump.sql
+++ database/dump.sql
@@ -350,6 +350,11 @@ COPY public."fcItem" ("fcId", "fcQuestion", "fcAnswer", "fcDeckId") FROM stdin;
 16	How do you indicate the grandparent folder in a path?	../../	1
 17	What are six primary HTML elements used for creating a table?	<table>, <thead>, <tbody>, <th>, <tr>, <td>	1
 18	What purpose to <thead> and <tbody> elements serve?	They help delineate which part of the table is the head and which part is the body	1
+19	here question 	anser 	1
+20	Here is a new Question	Here's a Answer	1
+21	Quick Question? 	QUICK ANSWER	1
+22	A New Question	Here's the Answer	1
+23	FLASH CARD QUESTION	FLASH CARD ANSWER	1
 \.
 
 
@@ -370,16 +375,17 @@ COPY public.notebooks ("notebookId", "studentId", "notebookName") FROM stdin;
 --
 
 COPY public.notes ("noteId", "notebookId", "createdAt", "noteTitle", "noteContent", "noteDifficulty", "noteResource", "noteCode") FROM stdin;
-23	1	2020-07-02 17:52:36.991685+00	Anchor Tags	An anchor tag is an HTML tag. It is used to define the beginning and end of a hypertext link. Search engines use the tag to determine the subject matter of the destination URL. Users click on the anchor text to reach the link target.	4	[]	{}
-24	1	2020-07-02 18:07:21.503484+00	HTML Elements	An HTML element is defined by a start tag, some content, and an end tag.\n\nex):\n\n<h1>My First Heading</h1>\n<p>My first paragraph.</p>	1	[]	{}
-25	1	2020-07-02 18:10:26.528675+00	HTML Forms	A webform, web form or HTML form on a web page allows a user to enter data that is sent to a server for processing. Forms can resemble paper or database forms because web users fill out the forms using checkboxes, radio buttons, or text fields	1	[]	{}
-26	1	2020-07-02 18:12:47.443626+00	Using CSS w/ HTML	CSS can be added to HTML documents in 3 ways:\n\nInline - by using the style attribute inside HTML elements\nInternal - by using a <style> element in the <head> section\nExternal - by using a <link> element to link to an external CSS file	1	[]	{}
-27	1	2020-07-02 18:15:01.504982+00	HTML Tables	The <table> tag defines an HTML table.\nEach table row is defined with a <tr> tag. Each table header is defined with a <th> tag. Each table data/cell is defined with a <td> tag.\nBy default, the text in <th> elements are bold and centered.\nBy default, the text in <td> elements are regular and left-aligned.	1	[]	{}
-28	1	2020-07-02 18:16:45.460257+00	HTML Skeleton	<!DOCTYPE html>\n<html>\n\n<head>\n  <meta charset="utf-8">\n  <title></title>\n  <meta name="author" content="">\n  <meta name="description" content="">\n  <meta name="viewport" content="width=device-width, initial-scale=1">\n\n  <link href="css/normalize.css" rel="stylesheet">\n  <link href="css/style.css" rel="stylesheet">\n</head>\n\n<body>\n\n  <p>Hello, world!</p>\n\n  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>\n  <script src="js/script.js"></script>\n</body>\n\n</html>\n	1	[]	{}
-29	1	2020-07-02 18:22:17.355235+00	What is HTML?	HTML stands for Hyper Text Markup Language\nHTML is the standard markup language for creating Web pages\nHTML describes the structure of a Web page\nHTML consists of a series of elements\nHTML elements tell the browser how to display the content\nHTML elements label pieces of content such as "this is a heading", "this is a paragraph", "this is a link", etc.	1	[]	{}
-30	1	2020-07-02 18:24:55.653418+00	HTML Lists	Ordered Lists: An ordered list starts with the <ol> tag. Each list item starts with the <li> tag.\n<ol>\n  <li>Coffee</li>\n  <li>Tea</li>\n  <li>Milk</li>\n</ol>\nUnordered Lists:  An unordered list starts with the <ul> tag. Each list item starts with the <li> tag.\n<ul>\n  <li>Coffee</li>\n  <li>Tea</li>\n  <li>Milk</li>\n</ul>	2	[]	{}
-31	1	2020-07-02 18:27:47.915352+00	HTML	Hypertext Markup Language (HTML) is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript and VBScript.	1	[]	{}
-32	1	2020-07-02 18:31:39.3803+00	HTML Paths	A file path specifies the location of a file inside a web folder structure. Its like an address of a file which helps the web browser to access the files. File paths are used to link external resources such as images, videos, style sheets, JavaScript, displaying other web pages etc.\n\nTo insert a file in a web page its source must be known. For example, the syntax (<img src=” ” alt=” “>) is used to insert an image file, where the path of the file is mentioned in the source (src).\nFile paths are of two types:\n\nAbsolute File Paths\nRelative File Paths	2	[]	{}
+25	1	2020-07-02 18:10:26.528675+00	HTML Forms	A webform, web form or HTML form on a web page allows a user to enter data that is sent to a server for processing. Forms can resemble paper or database forms because web users fill out the forms using checkboxes, radio buttons, or text fields	1	[{"link": "https://reactjs.org/docs/getting-started.html", "name": "HTML FRM Documenttation "}, {"link": "", "name": ""}, {"link": "", "name": ""}, {"link": "", "name": ""}]	{"js": "", "css": "h1{\\n  color: blue;\\n  border: 1px solid;\\n   ", "html": "<h1>Hi Dad and Robin<h1>"}
+26	1	2020-07-02 18:12:47.443626+00	Using CSS w/ HTML	CSS can be added to HTML documents in 3 ways:\n\nInline - by using the style attribute inside HTML elements\nInternal - by using a <style> element in the <head> section\nExternal - by using a <link> element to link to an external CSS file	1	[]	{"js": "", "css": "", "html": ""}
+27	1	2020-07-02 18:15:01.504982+00	HTML Tables	The <table> tag defines an HTML table.\nEach table row is defined with a <tr> tag. Each table header is defined with a <th> tag. Each table data/cell is defined with a <td> tag.\nBy default, the text in <th> elements are bold and centered.\nBy default, the text in <td> elements are regular and left-aligned.	1	[]	{"js": "", "css": "", "html": ""}
+24	1	2020-07-02 18:07:21.503484+00	HTML Elements	An HTML element is defined by a start tag, some content, and an end tag.\n\nex):\n\n<h1>My First Heading</h1>\n<p>My first paragraph.</p>	1	[]	{"js": "", "css": "", "html": ""}
+29	1	2020-07-02 18:22:17.355235+00	What is HTML?	HTML stands for Hyper Text Markup Language\nHTML is the standard markup language for creating Web pages\nHTML describes the structure of a Web page\nHTML consists of a series of elements\nHTML elements tell the browser how to display the content\nHTML elements label pieces of content such as "this is a heading", "this is a paragraph", "this is a link", etc.	1	[]	{"js": "", "css": "", "html": ""}
+31	1	2020-07-02 18:27:47.915352+00	HTML	Hypertext Markup Language (HTML) is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript and VBScript.	1	[]	{"js": "", "css": "", "html": ""}
+32	1	2020-07-02 18:31:39.3803+00	HTML Paths	A file path specifies the location of a file inside a web folder structure. Its like an address of a file which helps the web browser to access the files. File paths are used to link external resources such as images, videos, style sheets, JavaScript, displaying other web pages etc.\n\nTo insert a file in a web page its source must be known. For example, the syntax (<img src=” ” alt=” “>) is used to insert an image file, where the path of the file is mentioned in the source (src).\nFile paths are of two types:\n\nAbsolute File Paths\nRelative File Paths	2	[]	{"js": "", "css": "", "html": ""}
+28	1	2020-07-02 18:16:45.460257+00	HTML Skeleton	<!DOCTYPE html>\n<html>\n\n<head>\n  <meta charset="utf-8">\n  <title></title>\n  <meta name="author" content="">\n  <meta name="description" content="">\n  <meta name="viewport" content="width=device-width, initial-scale=1">\n\n  <link href="css/normalize.css" rel="stylesheet">\n  <link href="css/style.css" rel="stylesheet">\n</head>\n\n<body>\n\n  <p>Hello, world!</p>\n\n  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>\n  <script src="js/script.js"></script>\n</body>\n\n</html>\n	1	[]	{"js": "", "css": "", "html": ""}
+30	1	2020-07-02 18:24:55.653418+00	HTML Lists	Ordered Lists: An ordered list starts with the <ol> tag. Each list item starts with the <li> tag.\n<ol>\n  <li>Coffee</li>\n  <li>Tea</li>\n  <li>Milk</li>\n</ol>\nUnordered Lists:  An unordered list starts with the <ul> tag. Each list item starts with the <li> tag.\n<ul>\n  <li>Coffee</li>\n  <li>Tea</li>\n  <li>Milk</li>\n</ul>	2	[]	{"js": "", "css": "", "html": ""}
+23	1	2020-07-02 17:52:36.991685+00	Anchor Tags	An anchor tag is an HTML tag. It is used to define the beginning and end of a hypertext link. Search engines use the tag to determine the subject matter of the destination URL. Users click on the anchor text to reach the link target.	5	[]	{"js": "", "css": "", "html": ""}
+33	1	2020-07-03 17:52:30.104638+00	CSS Classes	The .class selector selects elements with a specific class attribute.\n\nTo select elements with a specific class, write a period (.) character, followed by the name of the class.\n\nYou can also specify that only specific HTML elements should be affected by a class. To do this, start with the element name, then write the period (.) character, followed by the name of the class.	5	[{"link": "https://www.w3schools.com/cssref/sel_class.asp", "name": "Documentation"}]	{"js": "document.querySelector('h1').textContent = 'Aloha!'", "css": "h1{\\n  color: blue;\\n  text-align: center;\\n  border: solid 1px ;\\n}", "html": "<h1>aloha</h1> "}
 \.
 
 
@@ -421,6 +427,31 @@ COPY public."tagRelations" ("itemId", type, "tagId") FROM stdin;
 16	fc	51
 17	fc	51
 18	fc	51
+24	note	1
+23	note	1
+33	note	26
+28	note	1
+30	note	1
+30	note	92
+34	note	51
+34	note	1
+34	note	7
+35	note	51
+35	note	1
+35	note	7
+36	note	51
+36	note	1
+36	note	7
+36	note	26
+37	note	51
+37	note	1
+38	note	51
+39	note	51
+19	fc	51
+20	fc	51
+21	fc	51
+22	fc	51
+23	fc	51
 \.
 
 
@@ -429,11 +460,15 @@ COPY public."tagRelations" ("itemId", type, "tagId") FROM stdin;
 --
 
 COPY public."tagTable" ("tagId", "tagName", "updatedAt") FROM stdin;
-26	CSS	2020-06-28 22:02:22.561273+00
-1	HTML	2020-06-30 04:41:15.787818+00
 6	variables	2020-06-30 04:41:15.787818+00
-7	javascript	2020-06-30 04:41:15.787818+00
-51		2020-07-02 18:57:11.657743+00
+92	Javascript	2020-07-03 18:13:33.239472+00
+7	javascript	2020-07-03 20:17:13.095649+00
+51		2020-08-11 01:18:02.607303+00
+26	CSS	2020-08-12 16:03:51.105053+00
+136	mowgli	2020-08-12 16:03:51.105053+00
+139	corey	2020-08-12 16:03:51.105053+00
+157	class	2020-08-12 16:03:51.105053+00
+1	HTML	2020-07-08 19:48:57.42473+00
 \.
 
 
@@ -448,7 +483,7 @@ SELECT pg_catalog.setval('public."fcDeck_fcDeckId_seq"', 1, true);
 -- Name: fcItem_fcId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public."fcItem_fcId_seq"', 18, true);
+SELECT pg_catalog.setval('public."fcItem_fcId_seq"', 23, true);
 
 
 --
@@ -462,7 +497,7 @@ SELECT pg_catalog.setval('public."notebooks_notebookId_seq"', 4, true);
 -- Name: notes_noteId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public."notes_noteId_seq"', 32, true);
+SELECT pg_catalog.setval('public."notes_noteId_seq"', 39, true);
 
 
 --
@@ -476,7 +511,7 @@ SELECT pg_catalog.setval('public."students_studentId_seq"', 1, true);
 -- Name: tagTable_tagId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public."tagTable_tagId_seq"', 82, true);
+SELECT pg_catalog.setval('public."tagTable_tagId_seq"', 179, true);
 
 
 --
